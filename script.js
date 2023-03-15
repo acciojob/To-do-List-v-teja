ebf11f28d9bb7f5cab8c66a78faffe81a3932cb2
Viswa Teja <115293958+v-teja@users.noreply.github.com>
2023-03-15 12:44:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+document.getElementById('addTodoBtn').addEventListener('click',
+			()=> {
+
+let input = document.getElementById("newTodoInput");
+				if(input.value != "")
+					
+				{const node = document.createElement("li");
+document.getElementById('todoList').appendChild(node).innerText=input.value;
+				input.value = "";}
+				else
+					window.alert("please input something!")
+				
+});
 
